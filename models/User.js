922ce8400423ddfadfa5c07187fa604a5654f433
unknown <km_si@ymail.com>
2021-04-10 19:56:18 -0400

    fix some code in user-controller

--- models/User.js
+++ models/User.js
@@ -1,48 +1,52 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought'
-            }
-          ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ]
-        },
-        {
-            toJSON: {
-              virtuals: true,
-              getters: true,
-            },
-            id: false,
-          }
-        );
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 //retrives the length of the user's friends
-UserSchema.virtual('friendCount').get(function() {
-    return this.friends.lenght;
-});
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });  
+  
 
 // create the user model using userSchema
-const User = model("User", UserSchema);
+const User = model("User", userSchema);
 
 // export the model
 module.exports = User;
