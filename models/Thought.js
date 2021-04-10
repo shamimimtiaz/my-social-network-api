7efa1c7cd4f087f540b9b68f07cf9f0f67d6146a
Shamim Imtiaz <km_si@ymail.com>
2021-04-10 12:58:20 -0400

    Create thought model and dateformat file and connect them

---
+++ models/Thought.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+          },
+        },
+        {
+          toJSON: {
+            getters: true,
+          },
+    }
+); 
+    
+const ThoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+// use reactionSchema to validate data for a reply
+      reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true,
+        },
+        id: false,
+      }
+    );
+
+//retrives the length of the thought's reactions
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
+  const Thought = model("Thought", ThoughtSchema);
+  
+  module.exports = Thought;
